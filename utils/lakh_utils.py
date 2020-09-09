fe0f4fb39f8dd61e15d940be646dd5b8cd02267a
Chris <e0946375@student.tuwien.ac.at>
2020-09-09 19:11:28 +0200

    Add functionality to retrieve score matches and MIDI MD5s

--- utils/lakh_utils.py
+++ utils/lakh_utils.py
@@ -1,7 +1,11 @@
 """Utilities to work with Lakh MIDI dataset. Mostly taken from
-https://nbviewer.jupyter.org/github/craffel/midi-dataset/blob/master/Tutorial.ipynb"""
+https://nbviewer.jupyter.org/github/craffel/midi-dataset/blob/master/Tutorial.ipynb
+and
+https://github.com/PacktPublishing/hands-on-music-generation-with-magenta"""
 
+import json
 from pathlib import Path
+from typing import Dict
 
 # Local path constants
 DATA_PATH = Path(__file__).parent.parent.absolute() / 'data'
@@ -24,3 +28,39 @@ def msd_id_to_h5(msd_id: str) -> Path:
 def get_midi_path(msd_id: str, midi_md5: str):
     """Given an MSD ID and MIDI MD5, return path to a MIDI file."""
     return Path(DATA_PATH, 'lmd_matched', msd_id_to_dirs(msd_id), midi_md5 + '.mid')
+
+
+def get_msd_score_matches(match_scores_path: Path = MATCH_SCORE_FILE) -> Dict:
+    """
+    Returns the dictionary of scores from the match scores file.
+
+    :param match_scores_path: the match scores path
+    :return: the dictionary of scores
+    """
+    with match_scores_path.open() as f:
+        return json.load(f)
+
+
+def get_matched_midi_md5(msd_id: str, msd_score_matches: dict, max_matched=True):
+    """
+    Returns the MD5 of the MIDI with the highest match from its MSD id.
+    :param msd_id: the MSD id
+    :param msd_score_matches: the MSD score dict, use get_msd_score_matches
+    :param max_matched: whether to only return the highest matched MIDI MD5 (default: true),
+    or a list of all matched MIDI MD5s (false)
+    :return: the matched MIDI MD5 if max_matched is True,
+    else, a list of matched MIDI MD5s
+    """
+    matched_midi_md5 = None
+    if max_matched:
+        max_score = 0
+        for midi_md5, score in msd_score_matches[msd_id].items():
+            if score > max_score:
+                max_score = score
+                matched_midi_md5 = midi_md5
+    else:
+        matched_midi_md5 = msd_score_matches[msd_id].keys()
+
+    if not matched_midi_md5:
+        raise Exception(f"Not matched {msd_id}: {msd_score_matches[msd_id]}")
+    return matched_midi_md5
