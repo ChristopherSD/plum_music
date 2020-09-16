235c6c31e6c5d07e4d7119ba3dfaf14be0f75838
Chris <e0946375@student.tuwien.ac.at>
2020-09-16 17:46:17 +0200

    Add multitrack models

--- utils/magenta_models_utils.py
+++ utils/magenta_models_utils.py
@@ -16,7 +16,6 @@ def download_checkpoint(model_name: str,
     :param model_name:
     :param checkpoint_name:
     :param target_dir:
-    :return:
     """
     tf.io.gfile.makedirs(target_dir)
     checkpoint_target = Path(target_dir) / checkpoint_name
@@ -30,8 +29,52 @@ def download_checkpoint(model_name: str,
             f.write(data)
 
 
-def get_model(name: str, batch_size: int = 8):
+def _get_multitrack_model(name: str = 'hier-multiperf_vel_1bar_med', batch_size: int = 8) -> TrainedModel:
+    """
+    Download the musicVAE multitrack model and returns a TrainedModel with the given batch size.
+    This model (hier-multiperf_vel_1bar_med_chords or hier-multiperf_vel_1bar_med) is not listed on the official
+    Github repository. The checkpoints have to be downloaded manually with the gsutil command line tool:
+    gsutil -m cp gs://download.magenta.tensorflow.org/models/music_vae/multitrack/* <output_dir>
+
+    :param name: One of 'hier-multiperf_vel_1bar_med_chords' or 'hier-multiperf_vel_1bar_med' (default).
+    Whether to load the chord conditioned or the unconditioned model.
+    :param batch_size: The batch size for the TrainedModel to build the model graph with.
+    :return: A musicVAE.TrainedModel with the given configuration.
+    """
     constants = get_constants_dict()
+
+    if "chords" in name:
+        checkpoint = Path(constants["MODELS_MUSICVAE_PATH"], "multitrack", "conditioned",
+                          constants["CHECKPOINT_MUSICVAE_MULTITRACK_CONDITIONED"])
+    else:
+        checkpoint = Path(constants["MODELS_MUSICVAE_PATH"], "multitrack", "unconditioned",
+                          constants["CHECKPOINT_MUSICVAE_MULTITRACK"])
+
+    return TrainedModel(
+        config=configs.CONFIG_MAP[name],
+        batch_size=batch_size,
+        checkpoint_dir_or_path=str(checkpoint)
+    )
+
+
+def get_model(name: str, batch_size: int = 8) -> TrainedModel:
+    """
+    Download the musicVAE model given by name and return a TrainedModel according to its config.
+    :param name: The name of the model retrieve. One of:
+    - cat-mel_2bar_big
+    - hierdec-trio_16bar
+    - hierdec-mel_16bar
+    - cat-drums_2bar_small.hikl
+    - hier-multiperf_vel_1bar_med_chords
+    - hier-multiperf_vel_1bar_med
+    :param batch_size: The batch size for the TrainedModel to build the model graph with.
+    :return: A musicVAE.TrainedModel with the given configuration.
+    """
+    constants = get_constants_dict()
+
+    if "multiperf" in name.lower():
+        model = _get_multitrack_model(name, batch_size)
+        return model
 
     checkpoint = name + ".tar"
     download_checkpoint("music_vae", checkpoint, constants["MODELS_MUSICVAE_PATH"])
