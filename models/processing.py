1641ec5b78f3ab3228151f3ee5a05239428e4d5d
Chris <e0946375@student.tuwien.ac.at>
2020-09-17 15:27:32 +0200

    Add main processing loop

--- models/processing.py
+++ models/processing.py
@@ -3,7 +3,7 @@ from typing import List
 
 import models.preprocessing as pre
 from config.config import get_constants_dict
-from utils.data_utils import get_all_songs_from_genre
+from utils.data_utils import get_all_songs_from_genre, get_all_songs_from_genres_of_size
 from utils.magenta_models_utils import get_model
 
 import numpy as np
@@ -107,3 +107,114 @@ def get_latent_encoding_for_genre_split(genre: str, model_name: str, n_bars: int
     )
 
     return encodings
+
+
+def get_encodings_for_genres_split(model_name: str):
+    genre_files = get_all_songs_from_genres_of_size()
+    model = get_model(model_name)
+    genres, encodings, msd_ids = [], [], []
+
+    num_successful, num_no_extracted, num_multiple_extracted, num_unsuccessful = 0, 0, 0, 0
+    no_extract, multi_extract, unsuccessful = {}, {}, {}
+    exceptions = []
+    for genre, files in genre_files.items():
+        for song_id, song_path in files:
+            try:
+                seq = ns.midi_file_to_note_sequence(str(song_path))
+                splits = pre.split_sequence_by_num_bars(seq)
+            except Exception as ex:
+                num_unsuccessful += 1
+                exceptions.append(ex)
+                continue
+            else:
+                for split in splits:
+                    try:
+                        z, _, _ = model.encode([split])
+                    except NoExtractedExamplesError as ex:
+                        num_no_extracted += 1
+                        no_extract[genre] = song_id
+                        continue
+                    except MultipleExtractedExamplesError as ex:
+                        num_multiple_extracted += 1
+                        multi_extract[genre] = song_id
+                        continue
+                    except Exception as ex:
+                        num_unsuccessful += 1
+                        unsuccessful[genre] = song_id
+                        exceptions.append(ex)
+                        continue
+                    else:
+                        encodings.append(z)
+                        genres.append(genre)
+                        msd_ids.append(song_id)
+                        num_successful += 1
+
+    print(exceptions)
+
+    num_fails = num_no_extracted + num_multiple_extracted + num_unsuccessful
+    num_files = sum(sum([v for v in genre_files.values()]))
+    print(
+        f"Extracted {num_successful} encodings from a total of {num_files} files",
+        f"Skipped {num_fails} files"
+        f"Distribution:\n"
+        f"\tNo Extracted Examples:{num_no_extracted}"
+        f"\tMultiple Extracted Examples:{num_multiple_extracted}"
+        f"\tOther exceptions:{num_unsuccessful}"
+    )
+
+    return genres, encodings, msd_ids
+
+
+def get_encodings_for_genres(model_name: str):
+    genre_files = get_all_songs_from_genres_of_size()
+    model = get_model(model_name)
+    genres, encodings, msd_ids = [], [], []
+
+    num_successful, num_no_extracted, num_multiple_extracted, num_unsuccessful = 0, 0, 0, 0
+    no_extract, multi_extract, unsuccessful = {}, {}, {}
+    exceptions = []
+    for genre, files in genre_files.items():
+        for song_id, song_path in files:
+            try:
+                seq = ns.midi_file_to_note_sequence(str(song_path))
+                splits = pre.split_sequence_by_num_bars(seq)
+            except Exception as ex:
+                num_unsuccessful += 1
+                exceptions.append(ex)
+                continue
+            else:
+                try:
+                    z, _, _ = model.encode([seq])
+                except NoExtractedExamplesError as ex:
+                    num_no_extracted += 1
+                    no_extract[genre] = song_id
+                    continue
+                except MultipleExtractedExamplesError as ex:
+                    num_multiple_extracted += 1
+                    multi_extract[genre] = song_id
+                    continue
+                except Exception as ex:
+                    num_unsuccessful += 1
+                    unsuccessful[genre] = song_id
+                    exceptions.append(ex)
+                    continue
+                else:
+                    encodings.append(z)
+                    genres.append(genre)
+                    msd_ids.append(song_id)
+                    num_successful += 1
+
+    print(exceptions)
+
+    num_fails = num_no_extracted + num_multiple_extracted + num_unsuccessful
+    num_files = sum(sum([v for v in genre_files.values()]))
+    print(
+        f"Extracted {num_successful} encodings from a total of {num_files} files",
+        f"Skipped {num_fails} files"
+        f"Distribution:\n"
+        f"\tNo Extracted Examples:{num_no_extracted}"
+        f"\tMultiple Extracted Examples:{num_multiple_extracted}"
+        f"\tOther exceptions:{num_unsuccessful}"
+    )
+
+    return genres, encodings, msd_ids
